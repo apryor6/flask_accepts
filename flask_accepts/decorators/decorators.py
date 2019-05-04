609be0bba4c5bc0d345ccc58aec33de96f5cb987
Alan Pryor <APryor@ATD-US.com>
2019-05-04 11:46:51 -0400

    Integrate marshmallow and unify errors

--- flask_accepts/decorators/decorators.py
+++ flask_accepts/decorators/decorators.py
@@ -1,17 +1,30 @@
-def accepts(*args):
+def accepts(*args, schema=None):
     from flask_restful import reqparse
-    parser = [arg for arg in args if isinstance(arg, reqparse.RequestParser)]
-    parser = (parser and parser[0]) or reqparse.RequestParser(bundle_errors=True)
+
+    _parser = reqparse.RequestParser(bundle_errors=True)
     for arg in args:
         if isinstance(arg, dict):
-            parser.add_argument(**arg)
+            _parser.add_argument(**arg)
 
     def decorator(func):
         from functools import wraps
         @wraps(func)
         def inner(*args, **kwargs):
             from flask import request
-            request.parsed_args = parser.parse_args()
+            error = {}
+            try:
+                request.parsed_args = _parser.parse_args()
+            except Exception as e:
+                # print('E = ', e)
+                error = e
+                # raise(e)
+            if schema:
+                obj, err = schema().load(request.get_json())
+                if err:
+                    error.data['message'].update({'schema_errors': err})
+                request.parsed_obj = obj
+            if error:
+                raise(error)
             return func(*args, **kwargs)
         return inner
     return decorator
