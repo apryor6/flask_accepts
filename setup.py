7a5d57e38847f0965a5e396e74c5a954ad943e71
Alan Pryor <APryor@ATD-US.com>
2019-06-05 08:03:08 -0400

    Increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(name='flask_accepts',
       author='Alan "AJ" Pryor, Jr.',
       author_email='apryor6@gmail.com',
-      version='0.2.0',
+      version='0.2.1',
       description='Easy Flask input validation',
       ext_modules=[],
       packages=find_packages(),
