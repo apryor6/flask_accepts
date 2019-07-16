ae570298f3c38c49f333249e3373113d892797f7
Alan Pryor <APryor@ATD-US.com>
2019-07-16 13:11:08 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(name='flask_accepts',
       description='Easy Flask input validation',
       ext_modules=[],
       packages=find_packages(),
-      install_requires=['Flask-RESTful>=0.3.7'],
+      install_requires=['Flask-RESTful>=0.3.7', "marshmallow>=2.19.5", "flask-restplus>=0.12.1"],
       )
