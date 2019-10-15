a9775785796c3dace66032d042f7435315a4af6c
AJ Pryor <apryor6@gmail.com>
2019-10-15 08:02:56 -0400

    Add example of using many=True in an app with flask_accepts

---
+++ examples/marshmallow_with_many_true.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass
+from marshmallow import fields, Schema, post_load
+from flask import Flask, jsonify, request
+from flask_accepts import accepts, responds
+
+
+@dataclass
+class Widget:
+    foo: str
+    baz: int
+
+
+class WidgetSchema(Schema):
+    foo = fields.String(default="test value")
+    baz = fields.Integer(default=422)
+
+    @post_load
+    def make(self, data, **kwargs):
+        return Widget(**data)
+
+
+def create_app(env=None):
+    from flask_restplus import Api, Namespace, Resource
+
+    app = Flask(__name__)
+    api = Api(app)
+
+    @api.route("/restplus/make_a_widget")
+    class WidgetResource(Resource):
+        @accepts(
+            dict(name="some_arg", type=str), schema=WidgetSchema(many=True), api=api
+        )
+        @responds(schema=WidgetSchema(many=True), api=api)
+        def post(self):
+            from flask import jsonify
+
+            return request.parsed_obj
+
+    return app
+
+
+app = create_app()
+if __name__ == "__main__":
+    app.run(debug=True)
