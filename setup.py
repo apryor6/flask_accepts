763bab7e8cf86281d385c8e5e7d5f8a27e27795b
apryor6 <apryor6@gmail.com>
2021-03-16 04:12:48 -0400

    Increment version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name="flask_accepts",
     author='Alan "AJ" Pryor, Jr.',
     author_email="apryor6@gmail.com",
-    version="0.17.6",
+    version="0.17.7",
     description="Easy, opinionated Flask input/output handling with Flask-restx and Marshmallow",
     ext_modules=[],
     packages=find_packages(),
